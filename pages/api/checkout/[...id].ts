c5c97e38cec6def5065e26666778978e16c6c9b1
rayansilva9 <raiansilvab082@gmail.com>
2023-08-18 13:07:26 -0400

    webhook

--- pages/api/checkout/[...id].ts
+++ pages/api/checkout/[...id].ts
@@ -1,12 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import Stripe from 'stripe'
+import client from '../../../lib/mongo'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2022-11-15'
 })
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
   if (req.method === 'POST') {
     try {
       console.log(req.headers.origin)
@@ -32,6 +32,18 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         success_url: `https://localhost/success?session_id=${req.headers.origin}`,
         cancel_url: `https://localhost/canceled?session_id=${req.headers.origin}`
       })
+      await client.connect()
+      const db = client.db('admin-loja')
+      const coll = db.collection('payment-intents')
+
+      await coll.insertOne({
+        id: session.id,
+        ammount_received: session.amount_total,
+        createdAt: session.created,
+        currency: session.currency,
+        status: 'criado'
+      })
+
       res.redirect(303, session.url)
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message)
